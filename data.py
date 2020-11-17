cdc71e1c58aec4e5d99eb3a4109c2de4e5be4ed3
rpartsey <partsey2412@gmail.com>
2020-11-17 15:48:13 +0200

    added EgoviewsDatasetV2

--- data.py
+++ data.py
@@ -29,3 +29,26 @@ class EgoviewsDataset(Dataset):
 
     def __len__(self):
         return self.df.shape[0]
+
+
+class EgoviewsDatasetV2(EgoviewsDataset):
+    def __getitem__(self, idx):
+        scene_name, episode_name, t = self.df.iloc[idx]
+        episode_dir = os.path.join(self.data_root, scene_name, episode_name)
+
+        poses_gt = np.load(os.path.join(episode_dir, 'poses_gt.npy'))
+        poses_est = np.load(os.path.join(episode_dir, 'poses_est.npy'))
+        pose_error = (self.step_delta(poses_gt, t) - self.step_delta(poses_est, t)).reshape(3, 1, 1)
+
+        observed_projection = np.load(os.path.join(episode_dir, 'egoview_projections', f'{str(t).zfill(3)}.npy'))
+        expected_projection = np.load(os.path.join(episode_dir, 'egoview_projections_prev', f'{str(t).zfill(3)}.npy'))
+
+        observed_projection = torch.from_numpy(observed_projection).type(torch.float32)
+        expected_projection = torch.from_numpy(expected_projection).type(torch.float32)
+        pose_error = torch.from_numpy(pose_error).type(torch.float32)
+
+        return (observed_projection, expected_projection), pose_error
+
+    @staticmethod
+    def step_delta(trajectory, t):
+        return trajectory[t] - trajectory[t - 1] if t > 0 else trajectory[t]
